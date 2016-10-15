aa74fdd0c057bf79f03f09e6bbd215ad9694e2c7
panagiotis916 <panayiotisscorps@hotmail.com>
2016-10-15 23:26:30 +0100

    changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,17 @@
-var dataset = [15,30,45,60,75];
+var dataset = [15,30,45,60];
 
+var idFirstChart = 0;
 d3.select("#chart-01").selectAll("div")
     .data(dataset)
     .enter()
     .append("div")
-    .attr("class", "bar")
+    .attr("class", function(){
+        var temp = "bar-"+idFirstChart;
+        idFirstChart++;
+        return temp;
+    })
     .style("background","cyan")
-    .style("height","19px")
+    .style("height","24px")
     .style("margin-bottom","1px")
     .style("width", function(d) {
         var barWidth = d * 5;
@@ -19,7 +24,7 @@ d3.select("#chart-02").selectAll("div")
     .append("div")
     .attr("class", "bar")
     .style("background","red")
-    .style("height","19px")
+    .style("height","24px")
     .style("margin-bottom","1px")
     .style("width", function(d) {
         var barWidth = d * 5;
@@ -32,7 +37,7 @@ d3.select("#chart-03").selectAll("div")
     .append("div")
     .attr("class", "bar")
     .style("background","yellow")
-    .style("height","19px")
+    .style("height","24px")
     .style("margin-bottom","1px")
     .style("width", function(d) {
         var barWidth = d * 5;
